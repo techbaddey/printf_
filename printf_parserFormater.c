cae806930f9f829f5791ef9a61a4b99376571e45
Amir <amirosama21@gmail.com>
2023-04-25 21:13:40 +0200

    updated with the betty indentation

--- printf_parserFormater.c
+++ printf_parserFormater.c
@@ -9,43 +9,43 @@
  */
 int parse_format(const char *format, format_t get_opt[], va_list valist)
 {
-    int i, j, count, total_characters = 0;
+	int i, j, count, total_characters = 0;
 
-    for (i = 0; format[i] != '\0'; i++)
-    {
-        if (format[i] == '%')
-        {
-            for (j = 0; get_opt[j].opt != NULL; j++)
-            {
-                if (format[i + 1] == get_opt[j].opt[0])
-                {
-                    count = get_opt[j].call_fun(valist);
-                    if (count == -1)
-                        return (-1);
-                    total_characters += count;
-                    break;
-                }
-            }
-            if (format[i] == '\0')
-                break;
-            if (get_opt[j].opt == NULL && format[i + 1] != ' ')
-            {
-                if (format[i + 1] != '\0')
-                {
-                    _putchar(format[i]);
-                    _putchar(format[i + 1]);
-                    total_characters = total_characters + 2;
-                }
-                else
-                    return (-1);
-            }
-            i = i + 1;
-        }
-        else
-        {
-            _putchar(format[i]);
-            total_characters++;
-        }
-    }
-    return (total_characters);
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			for (j = 0; get_opt[j].opt != NULL; j++)
+			{
+				if (format[i + 1] == get_opt[j].opt[0])
+				{
+					count = get_opt[j].call_fun(valist);
+					if (count == -1)
+						return (-1);
+					total_characters += count;
+					break;
+				}
+			}
+			if (format[i] == '\0')
+				break;
+			if (get_opt[j].opt == NULL && format[i + 1] != ' ')
+			{
+				if (format[i + 1] != '\0')
+				{
+					_putchar(format[i]);
+					_putchar(format[i + 1]);
+					total_characters = total_characters + 2;
+				}
+				else
+					return (-1);
+			}
+			i = i + 1;
+		}
+		else
+		{
+			_putchar(format[i]);
+			total_characters++;
+		}
+	}
+	return (total_characters);
 }
